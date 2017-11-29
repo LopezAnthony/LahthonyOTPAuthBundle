c99fe244d052f23f7e3359e1ff109f4b337e576e
Lahcen <larson920@gmail.com>
2017-11-29 17:24:18 +0100

    add comments on OTPManager

--- Manager/OTPManager.php
+++ Manager/OTPManager.php
@@ -6,12 +6,35 @@ use LahthonyOTPAuthBundle\Model\OTPAuthInterface;
 use OTPHP\TOTP;
 use ParagonIE\ConstantTime\Base32;
 
+/**
+ * Class OTPManager
+ * @package LahthonyOTPAuthBundle\Manager
+ */
 class OTPManager
 {
+    /**
+     * @var int
+     */
     private $period;
+
+    /**
+     * @var string
+     */
     private $digestAlgo;
+
+    /**
+     * @var integer
+     */
     private $digit;
+
+    /**
+     * @var string
+     */
     private $issuer;
+
+    /**
+     * @var string
+     */
     private $image;
 
     public function __construct($period, $digestAlgo, $digit, $issuer, $image)
@@ -24,6 +47,12 @@ class OTPManager
         $this->image = $image;
     }
 
+    /**
+     * Initialize and return a instance of TOTP
+     *
+     * @param OTPAuthInterface $user
+     * @return TOTP|void
+     */
     public function getOTPClient(OTPAuthInterface $user)
     {
         if(null === $user->getSecretAuthKey())
@@ -46,6 +75,11 @@ class OTPManager
         return $totp;
     }
 
+    /**
+     * generate secret key base 32
+     *
+     * @return string
+     */
     public function generateSecretKey()
     {
         $mySecret = trim(Base32::encodeUpper(random_bytes(128)), '=');
