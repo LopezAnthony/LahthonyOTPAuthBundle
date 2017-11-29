00d5755d9c762059e7e753c2c371c338345f46d5
Lahcen <larson920@gmail.com>
2017-11-29 16:29:11 +0100

    add unit test

--- Manager/OTPManager.php
+++ Manager/OTPManager.php
@@ -26,6 +26,11 @@ class OTPManager
 
     public function getOTPClient(OTPAuthInterface $user)
     {
+        if(null === $user->getSecretAuthKey())
+        {
+            return;
+        }
+
         $totp = TOTP::create(
             $user->getSecretAuthKey(),
             $this->period,
@@ -33,7 +38,7 @@ class OTPManager
             $this->digit
         );
 
-        (method_exists($user, 'getEmail')) ? $totp->setLabel($user->getEmail()) : $totp->setLabel($user->getUsername());
+        (method_exists($user, 'getEmail')) ? $totp->setLabel($user->getEmail()) : $totp->setLabel('');
 
         $totp->setIssuer($this->issuer);
         $totp->setParameter('image', $this->image);
---
+++ Tests/EventSubscriber/RegisterOTPAuthKeySubscriberTest.php
@@ -0,0 +1,81 @@
+<?php
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use LahthonyOTPAuthBundle\EventListener\RegisterOTPAuthKeySubscriber;
+use LahthonyOTPAuthBundle\Manager\OTPManager;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Created by PhpStorm.
+ * User: Etudiant
+ * Date: 29/11/2017
+ * Time: 15:29
+ */
+
+class RegisterOTPAuthKeySubscriberTest extends TestCase
+{
+    /**
+     * @var OTPManager
+     */
+    private $otpManager;
+
+    /**
+     * @var RegisterOTPAuthKeySubscriber
+     */
+    private $RegisterOTPAuthKeySubscriber;
+
+    /**
+     * @var Swift_Mailer
+     */
+    private $mailer;
+
+    /**
+     * @var LifecycleEventArgs
+     */
+    private $arg;
+
+
+    protected function setUp()
+    {
+        $mailer = $this->createMock(Swift_Mailer::class);
+        $otpManager = $this->createMock(OTPManager::class);
+
+        $this->RegisterOTPAuthKeySubscriber = new RegisterOTPAuthKeySubscriber($mailer, $otpManager);
+
+        $this->arg = $this->createMock(LifecycleEventArgs::class);
+        $this->mailer = $mailer;
+        $this->otpManager = $otpManager;
+    }
+
+    public function testWillReturnNullIfNotImplementOTPAuthInterface()
+    {
+        $result = $this->RegisterOTPAuthKeySubscriber->prePersist($this->arg);
+        $this->assertNull($result);
+    }
+
+    public function testWillReturnNullIf2FactorAuthNotActivated()
+    {
+        $object = $this->createMock(\LahthonyOTPAuthBundle\Model\OTPAuthInterface::class);
+        $object
+            ->expects($this->once())
+            ->method('getOtp2auth')
+            ->willReturn(false)
+        ;
+
+        $this->arg
+            ->method('getObject')
+            ->willReturn($object)
+        ;
+
+        $result = $this->RegisterOTPAuthKeySubscriber->prePersist($this->arg);
+        $this->assertNull($result);
+    }
+
+    protected function tearDown()
+    {
+        $this->RegisterOTPAuthKeySubscriber = '';
+        $this->arg = '';
+        $this->mailer = '';
+        $this->otpManager = '';
+    }
+}
