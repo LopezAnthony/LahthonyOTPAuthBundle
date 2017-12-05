ec9a1fb67a5b60704e547df9bdf69633367e333c
LopezAnthony <toto.lopez.45@gmail.com>
2017-12-05 16:29:35 +0000

    Apply fixes from StyleCI

--- Controller/RecoveryController.php
+++ Controller/RecoveryController.php
@@ -3,11 +3,10 @@
  * Created by PhpStorm.
  * User: Etudiant
  * Date: 05/12/2017
- * Time: 13:49
+ * Time: 13:49.
  */
 
 namespace LahthonyOTPAuthBundle\Controller;
-
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -15,6 +14,5 @@ class RecoveryController extends Controller
 {
     public function recoveryAction()
     {
-
     }
 }
